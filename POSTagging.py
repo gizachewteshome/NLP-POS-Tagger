cf1e4d776f6b72341eb300ccb771ba79c5e47bfd
Unknown <justinegc28@yahoo.com>
2019-03-03 00:48:01 +0800

    added files

---
+++ POSTagging.py
@@ -0,0 +1,310 @@
+import nltk
+import tensorflow as tf
+import keras
+from gensim.models import Word2Vec
+import multiprocessing
+import os
+from keras.initializers import Constant
+import matplotlib.pyplot as plt
+import keras.backend as K
+from keras.utils import plot_model
+from keras.callbacks import ModelCheckpoint
+def f1(y_true, y_pred):
+    y_pred = K.round(y_pred)
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    # tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+
+    p = tp / (tp + fp )
+    r = tp / (tp + fn )
+
+    f1 = 2*p*r / (p+r)
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return K.mean(f1)
+
+def ignore_class_accuracy(to_ignore=0):
+    def ignore_accuracy(y_true, y_pred):
+        y_true_class = K.argmax(y_true, axis=-1)
+        y_pred_class = K.argmax(y_pred, axis=-1)
+ 
+        ignore_mask = K.cast(K.not_equal(y_pred_class, to_ignore), 'int32')
+        matches = K.cast(K.equal(y_true_class, y_pred_class), 'int32') * ignore_mask
+        accuracy = K.sum(matches) / K.maximum(K.sum(ignore_mask), 1)
+        return accuracy
+    return ignore_accuracy
+
+tagged_sentences = nltk.corpus.treebank.tagged_sents()
+ 
+print(tagged_sentences[0])
+print("Tagged sentences: ", len(tagged_sentences))
+print("Tagged words:", len(nltk.corpus.treebank.tagged_words()))
+
+import numpy as np
+ 
+sentences, sentence_tags =[], [] 
+for tagged_sentence in tagged_sentences:
+    sentence, tags = zip(*tagged_sentence)
+    sentences.append(sentence)
+    sentence_tags.append(tags)
+print(sentences[:3])
+print(sentence_tags[5])
+ 
+# ['Lorillard' 'Inc.' ',' 'the' 'unit' 'of' 'New' 'York-based' 'Loews'
+#  'Corp.' 'that' '*T*-2' 'makes' 'Kent' 'cigarettes' ',' 'stopped' 'using'
+#  'crocidolite' 'in' 'its' 'Micronite' 'cigarette' 'filters' 'in' '1956'
+# '.']
+# ['NNP' 'NNP' ',' 'DT' 'NN' 'IN' 'JJ' 'JJ' 'NNP' 'NNP' 'WDT' '-NONE-' 'VBZ'
+#  'NNP' 'NNS' ',' 'VBD' 'VBG' 'NN' 'IN' 'PRP$' 'NN' 'NN' 'NNS' 'IN' 'CD'
+#  '.']]
+
+
+    
+from sklearn.model_selection import train_test_split
+ 
+train_sentences, test_sentences, train_tags, test_tags = train_test_split(sentences, sentence_tags, test_size=0.2)
+
+words, tags = set([]), set([])
+ 
+for s in train_sentences:
+    for w in s:
+        words.add(w.lower())
+
+for ts in train_tags:
+    for t in ts:
+        tags.add(t)
+
+word2index = {w: i + 2 for i, w in enumerate(list(words))}
+word2index['-PAD-'] = 0  # The special value used for padding
+word2index['-OOV-'] = 1  # The special value used for OOVs
+ 
+tag2index = {t: i + 1 for i, t in enumerate(list(tags))}
+tag2index['-PAD-'] = 0  # The special value used to padding
+ 
+#Declare Model Parameters
+cbow = 0
+skipgram = 1
+EMB_DIM = 300 #more dimensions, more computationally expensive to train
+min_word_count = 1
+workers = multiprocessing.cpu_count() #based on computer cpu count
+context_size = 7
+downsampling = 1e-3
+learning_rate = 0.025 #initial learning rate
+min_learning_rate = 0.025 #fixated learning rate
+num_epoch = 15
+
+w2v = Word2Vec(
+    sg = skipgram,
+    hs = 1, #hierarchical softmax
+    size = EMB_DIM,
+    min_count = min_word_count, 
+    workers = workers,
+    window = context_size, 
+    sample = downsampling, 
+    alpha = learning_rate, 
+    min_alpha = min_learning_rate
+)
+print('Vocabulary size: %d' % len(words))
+w2v.build_vocab(train_sentences)
+w2v.train(train_sentences,epochs=10,total_examples=w2v.corpus_count)
+words = list(w2v.wv.vocab)
+# save model in ASCII (word2vec) format
+filename = 'embedding_word2vec.txt'
+w2v.wv.save_word2vec_format(filename, binary=False)
+
+# ['Lorillard' 'Inc.' ',' 'the' 'unit' 'of' 'New' 'York-based' 'Loews'
+#  'Corp.' 'that' '*T*-2' 'makes' 'Kent' 'cigarettes' ',' 'stopped' 'using'
+#  'crocidolite' 'in' 'its' 'Micronite' 'cigarette' 'filters' 'in' '1956'
+# '.']
+# ['NNP' 'NNP' ',' 'DT' 'NN' 'IN' 'JJ' 'JJ' 'NNP' 'NNP' 'WDT' '-NONE-' 'VBZ'
+#  'NNP' 'NNS' ',' 'VBD' 'VBG' 'NN' 'IN' 'PRP$' 'NN' 'NN' 'NNS' 'IN' 'CD'
+#  '.']
+embeddings_index={}
+f=open(os.path.join('','embedding_word2vec.txt '),encoding="utf-8")
+for line in f:
+    values=line.split()
+    word=values[0]
+    coefs=np.asarray(values[1:])
+    embeddings_index[word]=coefs
+f.close()
+
+train_sentences_X, test_sentences_X, train_tags_y, test_tags_y = [], [], [], []
+
+num_words=len(word2index)+1
+embedding_matrix=np.zeros((num_words,EMB_DIM))
+print(word2index)
+for word,i in word2index.items():
+    if i>num_words:
+        continue
+    embedding_vector=embeddings_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i]=embedding_vector
+ 
+for s in train_sentences:
+    s_int = []
+    for w in s:
+        try:
+            s_int.append(word2index[w.lower()])
+        except KeyError:
+            s_int.append(word2index['-OOV-'])
+ 
+    train_sentences_X.append(s_int)
+
+for s in test_sentences:
+    s_int = []
+    for w in s:
+        try:
+            s_int.append(word2index[w.lower()])
+        except KeyError:
+            s_int.append(word2index['-OOV-'])
+ 
+    test_sentences_X.append(s_int)
+
+for s in train_tags:
+    train_tags_y.append([tag2index[t] for t in s])
+
+for s in test_tags:
+    test_tags_y.append([tag2index[t] for t in s])
+
+print(train_sentences_X[0])
+print(test_sentences_X[0])
+print(train_tags_y[0])
+print(test_tags_y[0])
+
+
+
+MAX_LENGTH = len(max(train_sentences_X, key=len))
+print(MAX_LENGTH)  # 271
+
+from keras.preprocessing.sequence import pad_sequences
+ 
+train_sentences_X = pad_sequences(train_sentences_X, maxlen=MAX_LENGTH, padding='post')
+test_sentences_X = pad_sequences(test_sentences_X, maxlen=MAX_LENGTH, padding='post')
+train_tags_y = pad_sequences(train_tags_y, maxlen=MAX_LENGTH, padding='post')
+test_tags_y = pad_sequences(test_tags_y, maxlen=MAX_LENGTH, padding='post')
+ 
+print(train_sentences_X[0])
+print(test_sentences_X[0])
+print(train_tags_y[0])
+print(test_tags_y[0])
+
+
+from keras.models import Sequential
+from keras.layers import Dense, CuDNNLSTM,LSTM, InputLayer, Bidirectional, TimeDistributed, Embedding, Activation,Dropout
+from keras.optimizers import Adam
+
+model = Sequential()
+embedding_layer=Embedding(num_words,EMB_DIM,embeddings_initializer=Constant(embedding_matrix),input_length=MAX_LENGTH,trainable=True)
+model.add(InputLayer(input_shape=(MAX_LENGTH, )))
+model.add(embedding_layer)
+model.add(Bidirectional(CuDNNLSTM(128, return_sequences=True)))
+model.add(Dropout(0.4))
+model.add(TimeDistributed(Dense(len(tag2index),activation="relu")))
+model.add(Activation('softmax'))
+ 
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(0.001),
+              metrics=["accuracy",f1,ignore_class_accuracy(0)])
+ 
+model.summary()
+plot_model(model, to_file='model.png')
+
+def to_categorical(sequences, categories):
+    cat_sequences = []
+    for s in sequences:
+        cats = []
+        for item in s:
+            cats.append(np.zeros(categories))
+            cats[-1][item] = 1.0
+        cat_sequences.append(cats)
+    return np.array(cat_sequences)
+
+
+cat_train_tags_y = to_categorical(train_tags_y, len(tag2index))
+print(cat_train_tags_y[0])
+
+history=model.fit(train_sentences_X, to_categorical(train_tags_y, len(tag2index)), batch_size=128, epochs=100,validation_data=(test_sentences_X, to_categorical(test_tags_y, len(tag2index))))
+
+# Plot training & validation accuracy values
+plt.figure()
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig("fake_acc.png")
+
+plt.figure( )
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig("loss.png")
+
+# Plot training & validation loss values
+plt.figure()
+plt.plot(history.history['f1'])
+plt.plot(history.history['val_f1'])
+plt.title('Model f1')
+plt.ylabel('F1')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig("f1.png")
+
+# Plot training & validation loss values
+plt.figure()    
+plt.plot(history.history['ignore_accuracy'])
+plt.plot(history.history['val_ignore_accuracy'])
+plt.title('Final Accuracy')
+plt.ylabel('Final Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig("real_acc.png")
+
+
+scores = model.evaluate(test_sentences_X, to_categorical(test_tags_y, len(tag2index)))
+print(model.metrics_names)   # acc: 99.09751977804825   
+print(scores)   # acc: 99.09751977804825
+
+model.save("model.h5")
+test_samples = [
+    "running is very important for me .".split(),
+    "I was running every day for a month .".split()
+]
+ 
+# [['running', 'is', 'very', 'important', 'for', 'me', '.'], ['I', 'was', 'running', 'every', 'day', 'for', 'a', 'month', '.']]
+ 
+test_samples_X = []
+for s in test_samples:
+    s_int = []
+    for w in s:
+        try:
+            s_int.append(word2index[w.lower()])
+        except KeyError:
+            s_int.append(word2index['-OOV-'])
+    test_samples_X.append(s_int)
+ 
+test_samples_X = pad_sequences(test_samples_X, maxlen=MAX_LENGTH, padding='post')
+
+
+predictions = model.predict(test_samples_X)
+def logits_to_tokens(sequences, index):
+    token_sequences = []
+    for categorical_sequence in sequences:
+        token_sequence = []
+        for categorical in categorical_sequence:
+            token_sequence.append(index[np.argmax(categorical)])
+ 
+        token_sequences.append(token_sequence)
+ 
+    return token_sequences
+
+
+final_pred=logits_to_tokens(predictions, {i: t for t, i in tag2index.items()})
+for x in range(len(test_samples)):
+    print(test_samples[x])
+    print(final_pred[x][0:len(test_samples[x])])
